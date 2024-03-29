534eebd854912ae27241da82f775496cda1b0516
Kevin Cooney <kcooney@gmail.com>
2024-03-28 19:31:09 -0700

    Add second slide

--- git/slides.js
+++ git/slides.js
@@ -45,15 +45,15 @@ class CommittingSlide {
             if (event.indexh == slideIndex) {
                 this.reset();
             }
-	});
-	    
+        });
+            
         Reveal.on('ready', event => this.initialize());
     }
 
     initialize() {
         this.gitgraph = createGraph(
             this.section.getElementsByClassName("graph-container")[0]);
-        this.button.addEventListener("click", this.onButtonClicked);
+        this.button.addEventListener("click", event => this.onButtonClicked());
         this.reset();
     }
 
@@ -61,14 +61,14 @@ class CommittingSlide {
         this.button.disabled = false;
         this.count = 0;        
         this.gitgraph.clear();
-	this.code.innerHTML = "$ git checkout main";
 
+        this.code.innerHTML = "$ git checkout main";
         this.main = this.gitgraph.branch("main");
         this.main.commit("Initial commit");
         moveHeadTag(this.main);
     }
 
-    onButtonClicked = event => {
+    onButtonClicked() {
         switch (++this.count) {
           case 1:
               this.code.innerHTML += "<br />$ git commit -a -m 'Add shooter'";
@@ -79,49 +79,69 @@ class CommittingSlide {
               this.code.innerHTML += "<br />$ git commit -a -m 'Shoot faster'";
               this.main.commit("Shoot faster");
               moveHeadTag(this.main);
-              this.button.disabled = true;
-              break;                
+              break;
            case 3:
-              this.code.innerHTML += "<br />$ git checout -b feature1<br />$ git commit -a 'Use limelight";
-              const feature1 = this.main.branch("feature1");
-              feature1.commit("Experimental limelight code");
-              moveHeadTag(feature1);
+              this.code.innerHTML += "<br />$ git commit -a -m 'Revert shoot faster'";
+              this.main.commit("Revert shoot faster");
+              moveHeadTag(this.main);
               this.button.disabled = true;
               break;
         }
     }
 }
 
-function slide2() {
-    const graphContainer = document.getElementById("slide1-graph-container");
-    gitgraph = createGraph(graphContainer);
+class BranchesSlide {
+    count = 0;
 
-    // Simulate git commands with Gitgraph API.
-    const main = gitgraph.branch("main");
-    main.commit("Initial commit");
+    constructor(slideIndex) {
+        this.section = document.getElementById("branches-slide");
+        this.button = this.section.getElementsByTagName("button")[0];
+        const gctr = this.section.getElementsByClassName("git-container")[0];
+        this.code = gctr.getElementsByTagName("code")[0];
 
-    const develop = main.branch("develop");
-    develop.commit("Add TypeScript");
-
-    const aFeature = develop.branch("a-feature");
-    aFeature
-        .commit("Make it work")
-        .commit("Make it fast");
-
-    develop.merge(aFeature);
-    develop.commit("Prepare v1");
-
-    main.merge(develop);
-    moveHeadTag(main)
-
-    const btn = document.getElementById("slide1-button");
-    const gctr = document.getElementsByClassName("git-container")[0]
-    const code = gctr.getElementsByTagName("code")[0]
-    function slide1AddCommit() {
-        code.innerHTML += "<br />$ git commit"
-        main.commit("More");
-        moveHeadTag(main);
-        btn.disabled = true;      
+        Reveal.on('slidechanged', event => {
+            if (event.indexh == slideIndex) {
+                this.reset();
+            }
+        });
+            
+        Reveal.on('ready', event => this.initialize());
     }
-    btn.addEventListener("click", slide1AddCommit);
+
+    initialize() {
+        this.gitgraph = createGraph(
+            this.section.getElementsByClassName("graph-container")[0]);
+        this.button.addEventListener("click", event => this.onButtonClicked());
+        this.reset();
+    }
+
+    reset() {
+        this.button.disabled = false;
+        this.count = 0;        
+        this.gitgraph.clear();
+
+        this.code.innerHTML = "$ git checkout main";
+        this.main = this.gitgraph.branch("main");
+        this.main.commit("Initial commit");
+
+        this.code.innerHTML += "<br />$ git commit -a -m 'Add shooter'";
+        this.main.commit("Add shooter");
+        moveHeadTag(this.main);
+    }
+
+    onButtonClicked() {
+        switch (++this.count) {
+          case 1:
+              this.code.innerHTML += "<br />$ git checkout -b chicken/shoot-faster";
+              this.feature = this.main.branch("chicken/shoot-faster");
+              this.code.innerHTML += "<br />$ git commit -a -m 'Shoot faster'";
+              this.feature.commit("Shoot faster");
+              moveHeadTag(this.feature);
+              break;                
+          case 2:
+              this.main.commit("Add drive subsystem");
+              this.button.disabled = true;
+              break;
+        }
+    }
 }
