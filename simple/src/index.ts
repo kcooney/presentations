2cdbc68c616cab3dd07665bfb6fe41195932f93d
Kevin Cooney <kcooney@gmail.com>
2025-01-15 17:44:20 -0800

    Initial version of simple desk

---
+++ simple/src/index.ts
@@ -0,0 +1,10 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+Reveal.initialize({
+    plugins: [ Markdown ],
+    width: 1160,
+    center: false,
+    keyboard: true,
+    markdown: {},
+});
