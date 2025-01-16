3cf998bc24cf0ac6ce900fe57f4e3f0c5d56aa90
Kevin Cooney <kcooney@gmail.com>
2025-01-15 20:05:06 -0800

    Add reveal.js-menu

--- simple/src/index.ts
+++ simple/src/index.ts
@@ -1,10 +1,15 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealMenu from 'reveal.js-menu';
+import 'reveal.js-menu';
 
-Reveal.initialize({
-    plugins: [ Markdown ],
-    width: 1160,
-    center: false,
-    keyboard: true,
-    markdown: {},
+Reveal.initialize( {
+  plugins: [ Markdown, RevealMenu ],
+  width: 1160,
+  center: false,
+  keyboard: true,
+  markdown: {},
+  menu: {
+     path: "reveal.js-menu/"
+  }
 });
