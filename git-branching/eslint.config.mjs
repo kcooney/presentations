b69490db51cfeba30d56b819318965f4960fbf38
Kevin Cooney <kcooney@gmail.com>
2025-01-22 21:41:22 -0800

    Run prettier on eslint.config.mjs

--- git-branching/eslint.config.mjs
+++ git-branching/eslint.config.mjs
@@ -1,33 +1,33 @@
 // @ts-check
 
 import globals from "globals";
-import eslint from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   prettierConfig,
   {
-    "files": ["**/*.ts"],
-    "languageOptions": { globals: globals.browser },
-    "rules": {
+    files: ["**/*.ts"],
+    languageOptions: { globals: globals.browser },
+    rules: {
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "caughtErrors": "all",
-          "caughtErrorsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "ignoreRestSiblings": true
-        }
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
       ],
       "no-empty-function": "off",
       "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
     },
-  }
+  },
 );
